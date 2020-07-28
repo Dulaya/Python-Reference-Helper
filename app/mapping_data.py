40990bfac27314aa3bb60ebd210f7c1987135852
Dulaya Saennok <dulaya.saennok@gmail.com>
2020-07-27 20:45:44 -0700

    Updated training_data.py, mapping_data.py, styles.css including globa/local variables, random, integer, float, string, index list, slice list, length list and check if in list

--- app/mapping_data.py
+++ app/mapping_data.py
@@ -1,6 +1,126 @@
 mapping_data = {
 
+    "integer": 
+    [
+        """int() converts input into integer and truncate the decimals
+        """,
+"""print( int(1.9) )
+print( int('2') )
+print( int(-3.7) )"""
+    ],
 
+        "float": 
+    [
+        """float() converts input into float number 
+        """,
+"""print( float(1.99) )
+print( float('2') )
+print( float(-3.7) )"""
+    ],
+
+        "string": 
+    [
+        """str() converts input into string. Strings can be concatenated (adding) together. 
+        """,
+"""print( str(1.99) )
+print( str('2') )
+print( str('hello')  + ' ' + 'world' ) #string concatenation"""
+    ],
+
+    "global local variable": 
+    [
+        """Global variables are variable created outside of functions and exist (can be used) by every functions. 
+        Conversely, local variables are created inside of a function and can only be used inside the respective function.
+        The keyword <i>global</i> can be used to create a global variable inside of a function.
+        """,
+"""global_var = 'I am global.'
+
+def funct():
+    local_var = 'I am local.'
+
+    print(local_var)
+    print(global_var)
+
+def funct_2():
+    global global_var_2
+    global_var_2 = 'I am global too.'
+
+funct()
+funct_2()
+print(global_var)
+print(global_var_2)
+#print(local_var)"""
+    ],
+
+    "random number": 
+    [
+        """To generate random numbers, import random and use randrange.
+        """,
+"""import random
+
+x = random.randrange(0,9)
+y = random.randrange(0,9)
+print(x)
+print(y)
+"""
+    ],
+
+#################################################### List ################################################################
+"index list": 
+[
+"""Get element of list using index. Using negative index to get element from end of list.
+""",
+"""list = ['fish', 'donkey', 'shark', 'whale']
+print(list[1])
+print(list[-1])
+print(list[-2])
+"""
+],
+
+"slice list": 
+[
+"""Use slice to get range of list.
+""",
+"""list = ['fish', 'donkey', 'shark', 'whale']
+print(list[1:3])
+print(list[1:])
+"""
+],
+
+"length list": 
+[
+"""Use <i>len()</i> to get list of list.
+""",
+"""list = ['fish', 'donkey', 'shark', 'whale']
+print(len(list))
+"""
+],
+
+"if in list": 
+[
+"""Check if element is in list.
+""",
+"""list = ['fish', 'donkey', 'shark', 'whale']
+if 'fish' in list: print('fish is in list')
+if 'sheep' in list: print('sheep is in list')
+else: print('sheep is not in list')
+"""
+],
+
+    "append element list": 
+    [
+        """
+        list.append(<i>element</i>)<br/>
+        <br/>
+        list = ['a', 'b', 'c', 'd', 'e']<br/>
+        list.append( 'f' )<br/>
+        print( list )<br/>
+        >> ['a', 'b', 'c', 'd', 'e', 'f']
+        """,
+        """list = ['a', 'b', 'c', 'd', 'e']
+list.append( 'f' )
+print( list )"""
+    ],
 
     "delete element list": 
     [
@@ -50,20 +170,7 @@ list.remove('c')
 print( list )"""
     ],
     
-    "append element list": 
-    [
-        """
-        list.append(<i>element</i>)<br/>
-        <br/>
-        list = ['a', 'b', 'c', 'd', 'e']<br/>
-        list.append( 'f' )<br/>
-        print( list )<br/>
-        >> ['a', 'b', 'c', 'd', 'e', 'f']
-        """,
-        """list = ['a', 'b', 'c', 'd', 'e']
-list.append( 'f' )
-print( list )"""
-    ],
+
 
     
 }
--- app/training_data.py
+++ app/training_data.py
@@ -2,6 +2,48 @@
 
 training_data = [
 
+{"word": "integer", "key": "integer"},
+{"word": "int", "key": "integer"},
+{"word": "int number", "key": "integer"},
+
+{"word": "float", "key": "float"},
+{"word": "float number", "key": "float"},
+{"word": "floating", "key": "float"},
+
+{"word": "string", "key": "string"},
+{"word": "str", "key": "string"},
+
+{"word": "global variable", "key": "global local variable"},
+{"word": "global", "key": "global variable"},
+{"word": "local variable", "key": "global local variable"},
+{"word": "local", "key": "global local variable"},
+
+{"word": "random", "key": "random number"},
+{"word": "random number", "key": "random number"},
+
+#List
+{"word": "index", "key": "index list"},
+{"word": "index list", "key": "index list"},
+
+{"word": "slice", "key": "slice list"},
+{"word": "slice list", "key": "slice list"},
+
+{"word": "length list", "key": "length list"},
+{"word": "len list", "key": "length list"},
+{"word": "len() list", "key": "length list"},
+
+{"word": "check if in list", "key": "if in list"},
+{"word": "if in list", "key": "if in list"},
+
+{"word": "add element list", "key": "append element list"},
+{"word": "add element array", "key": "append element list"},
+{"word": "add value list", "key": "append element list"},
+{"word": "add value array", "key": "append element list"},
+{"word": "append element list", "key": "append element list"},
+{"word": "append element array", "key": "append element list"},
+{"word": "append value list", "key": "append element list"},
+{"word": "append value array", "key": "append element list"},
+
 {"word": "delete element list", "key": "delete element list"},
 {"word": "delete element array", "key": "delete element list"},
 {"word": "delete value list", "key": "delete element list"},
@@ -18,14 +60,9 @@ training_data = [
 {"word": "pop value list", "key": "pop element list"},
 {"word": "pop value array", "key": "pop element list"},
 
-{"word": "add element list", "key": "append element list"},
-{"word": "add element array", "key": "append element list"},
-{"word": "add value list", "key": "append element list"},
-{"word": "add value array", "key": "append element list"},
-{"word": "append element list", "key": "append element list"},
-{"word": "append element array", "key": "append element list"},
-{"word": "append value list", "key": "append element list"},
-{"word": "append value array", "key": "append element list"},
+
+
+
 
 ]
 
